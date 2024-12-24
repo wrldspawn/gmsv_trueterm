b5645f60428dd9226999eaa04591ea6b82118e99
worldspawn <worldspawn@riseup.net>
2024-12-24 00:21:31 -0700

    copypaste moment

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include <ByteBuffer.hpp>
 #include <GarrysMod/FactoryLoader.hpp>
 #include <GarrysMod/Lua/Interface.h>
 #include <Platform.hpp>
@@ -31,7 +30,7 @@ void RecolorMsg(int &RED, int &GREEN, int &BLUE) {
 #if ARCHITECTURE_IS_X86_64
 class TruetermListener : public ILoggingListener {
 public:
-	TruetermListener(const bQuietPrintf = false, bool bQuietDebugger = false) {}
+  TruetermListener(const bQuietPrintf = false, bool bQuietDebugger = false) {}
 
   void Log(const LoggingContext_t *pContext, const char *msg) override {
     int RED = pContext->m_Color.r;
@@ -69,93 +68,93 @@ public:
       }
     }
 
-		char buf[20] = "";
-		snprintf(buf, sizeof(buf), "\x1b[38;2;%03d;%03d;%03dm", RED, GREEN, BLUE);
+    char buf[20] = "";
+    snprintf(buf, sizeof(buf), "\x1b[38;2;%03d;%03d;%03dm", RED, GREEN, BLUE);
 
-		printf("%s", buf);
-		printf("%s", msg);
-		printf("\x1b[0m");
-	}
+    printf("%s", buf);
+    printf("%s", msg);
+    printf("\x1b[0m");
+  }
 };
 
-ILoggingListener* listener = new TruetermListener();
+ILoggingListener *listener = new TruetermListener();
 #else
 SpewOutputFunc_t spewFunction = nullptr;
 SpewRetval_t TruetermSpew(SpewType_t Type, const char *msg) {
-	SpewRetval_t ret;
+  SpewRetval_t ret;
 
-	if (!msg || *msg == '\0')
-		return SPEW_CONTINUE;
+  if (!msg || *msg == '\0')
+    return SPEW_CONTINUE;
 
-	const Color* col = GetSpewOutputColor();
+  const Color *col = GetSpewOutputColor();
 
-	int RED = 0;
-	int GREEN = 0;
-	int BLUE = 0;
-	int ALPHA = 0;
+  int RED = 0;
+  int GREEN = 0;
+  int BLUE = 0;
+  int ALPHA = 0;
 
-	col->GetColor(RED, GREEN, BLUE, ALPHA);
+  col->GetColor(RED, GREEN, BLUE, ALPHA);
 
-	bool printcol = RED > 0 || GREEN > 0 || BLUE > 0;
-	bool white = RED == 255 && GREEN == 255 && BLUE == 255;
+  bool printcol = RED > 0 || GREEN > 0 || BLUE > 0;
+  bool white = RED == 255 && GREEN == 255 && BLUE == 255;
 
-	if (Type == SPEW_WARNING && white) {
-		RED = 255;
-		GREEN = 100;
-		BLUE = 50;
-	} else if (Type == SPEW_LOG&& white) {
-		RED = 200;
-		GREEN = 235;
-		BLUE = 255;
-	} else if ((Type == SPEW_ERROR || Type == SPEW_ASSERT) && white) {
-		RED = 255;
-		GREEN = 90;
-		BLUE = 90;
-	}
+  if (Type == SPEW_WARNING && white) {
+    RED = 255;
+    GREEN = 100;
+    BLUE = 50;
+  } else if (Type == SPEW_LOG && white) {
+    RED = 200;
+    GREEN = 235;
+    BLUE = 255;
+  } else if ((Type == SPEW_ERROR || Type == SPEW_ASSERT) && white) {
+    RED = 255;
+    GREEN = 90;
+    BLUE = 90;
+  }
 
-	RecolorMsg(RED, GREEN, BLUE);
+  RecolorMsg(RED, GREEN, BLUE);
 
-	if (!printcol) {
-		if (Type != SPEW_MESSAGE) {
-			RED = 255;
-			BLUE = 255;
-		} else {
-			return SPEW_CONTINUE;
-		}
-	}
+  if (!printcol) {
+    if (Type != SPEW_MESSAGE) {
+      RED = 255;
+      BLUE = 255;
+    } else {
+      return SPEW_CONTINUE;
+    }
+  }
 
-	char buf[20] = "";
+  char buf[20] = "";
   snprintf(buf, sizeof(buf), "\x1b[38;2;%03d;%03d;%03dm", r, g, b);
 
   spewFunction(Type, buf);
   spewFunction(Type, msg);
   spewFunction(Type, "\x1b[0m");
 
-	return SPEW_CONTINUE;
+  return SPEW_CONTINUE;
 }
 #endif
 
 GMOD_MODULE_OPEN() {
 #if ARCHITECTURE_IS_X86_64
-	LoggingSystem_PushLoggingState(false, false);
-	LoggingSystem_RegisterLoggingListener(listener);
+  LoggingSystem_PushLoggingState(false, false);
+  LoggingSystem_RegisterLoggingListener(listener);
 #else
-	spewFunction = GetSpewOutputFunc();
-	SpewOutputFunc(TruetermSpew);
+  spewFunction = GetSpewOutputFunc();
+  SpewOutputFunc(TruetermSpew);
 
 #endif
 
-	return 0;
+  return 0;
 }
 
 GMOD_MODULE_CLOSE() {
 #if ARCHITECTURE_IS_X86_64
-	LoggingSystem_UnregisterLoggingListener(listener);
-	LoggingSystem_PopLoggingState(false);
-	delete listener;
+  LoggingSystem_UnregisterLoggingListener(listener);
+  LoggingSystem_PopLoggingState(false);
+  delete listener;
 #else
-	SpewOutputFunc(spewFunction);
+  SpewOutputFunc(spewFunction);
 #endif
 
-	return 0;
+  return 0;
 }
